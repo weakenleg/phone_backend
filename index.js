c7d2c1e36aec298cfe2237b87baa33c885e46322
qingyun guo <qingyun.guo@aalto.fi>
2023-06-21 22:58:42 +0300

    Update repository with new changes

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.get('/info', (request, response) => {
   
   app.use(unknownEndpoint)
 
-  const PORT = process.env.PORT || 3001
+  const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
