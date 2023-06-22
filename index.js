d5344dcce6463f9e618461fd4ed0bb7e8c19defd
qingyun guo <qingyun.guo@aalto.fi>
2023-06-22 20:37:02 +0300

    Update repository with new changes

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const http = require('http');
 const morgan = require('morgan');
 const cors = require('cors');
 app.use(cors());
+app.use(express.static('build'));
+const baseUrl = '/api/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
+    console.log('Request Method:', request.method)
+    console.log('Request Path:  ', request.path)
+    console.log('Request Body:  ', request.body)
     console.log('---')
     next()
 }
@@ -57,53 +64,53 @@ app.get('/info', (request, response) => {
     
     response.send(infoHtml);
   });
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-  
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).json({ error: 'Person not found' });
-    }
-  });
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-  
-    response.status(204).end();
-  });
-  app.post('/api/persons', (request, response) => {
-    const { name, number } = request.body;
-  
-    // Check if name or number is missing
-    if (!name || !number) {
-      return response.status(400).json({ error: 'name or number is missing' });
-    }
-  
-    // Check if name already exists in the phonebook
-    const existingPerson = persons.find((person) => person.name === name);
-    if (existingPerson) {
-      return response.status(400).json({ error: 'name must be unique' });
-    }
-  
-    const newPerson = {
-      id: Math.floor(Math.random() * 1), // Generate a random ID
-      name,
-      number,
-    };
-  
-    // Add the new phonebook entry to the list of entries
-    persons.push(newPerson);
-  
-    response.status(200).json(persons);
-  });
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).json({ error: 'Person not found' });
+  }
+});
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body;
+
+  // Check if name or number is missing
+  if (!name || !number) {
+    return response.status(400).json({ error: 'name or number is missing' });
+  }
+
+  // Check if name already exists in the phonebook
+  const existingPerson = persons.find((person) => person.name === name);
+  if (existingPerson) {
+    return response.status(400).json({ error: 'name must be unique' });
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 1), // Generate a random ID
+    name,
+    number,
+  };
+
+  // Add the new phonebook entry to the list of entries
+  persons.push(newPerson);
+
+  response.status(200).json(persons);
+});
   
 
   
-  app.use(unknownEndpoint)
+app.use(unknownEndpoint)
 
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
