c67635fd78d6eb654ec957b65096501bbc118d02
qingyun guo <qingyun.guo@aalto.fi>
2023-06-24 18:18:18 +0300

    Remove .env from tracking

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const url =
-  `mongodb+srv://qingyunguo:q87237151@cluster0.bdecck4.mongodb.net/?
-  retryWrites=true&w=majority`
+  `mongodb+srv://123456:654123@cluster0.5vhtud0.mongodb.net/
+  ?retryWrites=true&w=majority`
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
     console.log('Connected to MongoDB');
